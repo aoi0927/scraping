6058a9ff4b3c12a5c92c700f16b03a0d7dec8c8c
aoi0927 <59475415+aoi0927@users.noreply.github.com>
2022-04-24 12:40:25 +0900

    main

--- scraping.py
+++ scraping.py
@@ -1,16 +1,22 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import csv
 
 print('スクレイピングしたいurlを入力')
 url = input('>> ')
 html = urllib.request.urlopen(url).read().decode('UTF-8')  
 soup= BeautifulSoup(html, "lxml")
 
-tbody = soup.find_all('tr', {'class':'ek-hour_line'})
+data = soup.find_all('tr', {'class':'ek-hour_line'})
 
-for i in tbody:
+station = soup.find('div', {'class': 'station-name'}).text.split()[0][:-1]
+
+for i in data:
     hour = i.find('td')
+    print(hour)
     min = i.find_all('span', {'class': 'time-min means-text'})
-    a = i.find_all('li', {'class':'ek-tooltip ek-narrow ek-train-tooltip'})
+    min2 = i.find_all('span', {'class': 'time-min means-text start'})
+    min.extend(min2)
+    detail_data = i.find_all('li', {'class':'ek-tooltip ek-narrow ek-train-tooltip'})
     for j, m in enumerate(min):
-        print(f'{hour.text}:{m.text} {a[j]["data-tr-type"]} {a[j]["data-dest"]}行き')
+        print(f'{hour.text}:{m.text} {detail_data[j]["data-tr-type"]} {detail_data[j]["data-dest"]}行き')
